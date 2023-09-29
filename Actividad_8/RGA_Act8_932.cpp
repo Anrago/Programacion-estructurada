2e2c0a4bb1574a16448cb2dcb11412bec9d9a51a
Anrago <141792891+Anrago@users.noreply.github.com>
2023-09-28 22:45:26 -0700

    first commit

--- Actividad_8/RGA_Act8_932.cpp
+++ Actividad_8/RGA_Act8_932.cpp
@@ -5,12 +5,15 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
 int msg();
 void menu();
-void vect_1(int vect1[], int lim_in, int lim_sup);
+int validar(const char msg[], int li_in, int li_sup);
+void vect_1(int vect1[], int m);
 void vect_2(int vect2[], int lim_in, int lim_sup);
-void vect_3();
+void vect_3(int vect1[],int vect2[],int vect3[] ,int m);
+void imprimir_vetores(int vect1[],int vect2[],int vect3[] ,int m,int n);
 
 int main()
 {
@@ -27,6 +30,7 @@ int msg()
     printf("1.- LLENA VECTOR 1(MANUALMENTE)\n");
     printf("2.- LLENA VECTOR 2(ALEATORIAMENTE)\n");
     printf("3.- LLENA VECTOR 3(VECTOR 1 Y VECTOR 2)\n");
+    printf("4.- IMPRIMIR VECTOES\n");
     printf("ESCOGE UNA OPCION: ");
     scanf("%hd", &opc);
     return opc;
@@ -34,49 +38,72 @@ int msg()
 
 void menu()
 {
-    int vect1[10], vect2[10];
-    short int opc, i;
+    int vect1[10], vect2[10],vect3[20];
+    short int opc;
     do
     {
         opc = msg();
         switch (opc)
         {
         case 1:
-            vect_1(vect1, 30, 70);
+            vect_1(vect1, 10);
+            
             break;
         case 2:
             vect_2(vect2, 1, 20);
             break;
+        case 3:
+            vect_3(vect1,vect2,vect3,20);
+            break;
+        case 4:
+            imprimir_vetores(vect1,vect2,vect3,10,20);
         default:
             break;
         }
 
     } while (opc != 0);
 }
-void vect_1(int vect1[], int lim_in, int lim_sup)
+
+int validar(const char msg[], int li_in, int li_sup)
 {
-    char val[2];
-    int i,valid;
+    char cad[3];
+    int num;
+    
+    do
+    {
+        printf("%s", msg);
+        fflush(stdin);
+        gets(cad);
+        num=atoi(cad);
+
+        if(num<li_in || li_sup)
+        {
+            system("CLS");
+            printf("SOLO NUMEROS DEL 30 AL 70\n");
+        }
+    }while(num<li_in || num>li_sup);
+    return num;
+}
+
+void vect_1(int vect1[], int m)
+{
+    int i,val;
     system("CLS");
     printf("LLENA EL VECTOR\n");
-    printf("SOLO NUMEROS DEL %d al %d\n", lim_in, lim_sup);
-
+    printf("SOLO NUMEROS DEL 30 al 70\n");
+    system("PAUSE");
+    for (i = 0; i < m; i++)
+    {
+        system("CLS");
+        printf("Vec[%d]",i+1);
+        val=validar(" =  ",30,70);
+        vect1[i]=val;
+    }
     system("CLS");
-    printf("vect[%d]=", i + 1);
-    gets(val);
-    for (i = 0; i < 10; i++)
-    {
-        valid=atoi(val);
-    }
-
-    if (vect1[i] < lim_in)
-    {
-        printf("NUMERO MENOR NO VALIDO. INTENTE DENUEVO:\n");
-    }
-    if (vect1[i] > lim_sup)
-    {
-        printf("NUMERO MAYOR NO VALIDO. INTENTE DENUEVO:\n");
-    }
+    printf("**************************\n");
+    printf("El vector ha sido llenado\n");
+    printf("**************************\n");
+    system("PAUSE");
 }
 
 void vect_2(int vect2[], int lim_in, int lim_sup)
@@ -103,4 +130,71 @@ void vect_2(int vect2[], int lim_in, int lim_sup)
 
         vect2[i] = ran;
     }
+    system("CLS");
+    printf("**************************\n");
+    printf("El vector ha sido llenado\n");
+    printf("**************************\n");
+    system("PAUSE");
 }
+
+void vect_3(int vect1[],int vect2[], int vect3[],int m)
+{
+    int i,j;
+    system("CLS");
+    for (i=0,j=10; i<m/2;i++,j++)
+    {
+        vect3[i]=vect1[i];
+        vect3[j]=vect2[i];
+    }
+    
+    printf("**************************\n");
+    printf("El vector ha sido llenado\n");
+    printf("**************************\n");
+    system("PAUSE");
+
+}
+
+void imprimir_vetores(int vect1[],int vect2[],int vect3[] ,int m,int n)
+{
+    int i;
+    system("CLS");
+    printf("ARREGLO 1\n");
+    printf("---------------------\n");
+    for(i=0;i<m;i++)
+    {
+        printf("VECT[%d]=\t%d\n",i,vect1[i]);
+    }
+    printf("---------------------");
+    printf("\nARREGLO 2\n");
+    printf("---------------------\n");
+
+    for(i=0;i<m;i++)
+    {
+        printf("VECT[%d]=\t%d\n",i,vect2[i]);
+    }
+    printf("---------------------");
+    printf("\nARREGLO 3\n");
+    printf("---------------------\n");
+    for(i=0;i<n;i++)
+    {
+        printf("VECT[%d]=\t%d\n",i,vect3[i]);
+    }
+    system("PAUSE");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
